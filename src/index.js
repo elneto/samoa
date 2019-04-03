6d48b895f9a0fa965044ed17b1ea4a4c3fc7d347
Ernesto <araiza@un.org>
2019-04-03 10:42:20 -0400

    changes to jsbeautifyrc

--- src/index.js
+++ src/index.js
@@ -2,19 +2,16 @@ import 'bootstrap';
 import './style.scss';
 var sdgkplogo = document.getElementById('sdgsummitLogo');
 var $ = require('jquery');
-
 function headerSmall () {
 	sdgkplogo.className = 'sdgkplogo_small';
 	$('button.navbar-toggler').addClass('mt-3').removeClass('mt-5');
 	$('form#search-form').addClass('mt-3').removeClass('mt-5');
 };
-
 function headerNormal () {
 	sdgkplogo.className = 'sdgkplogo_normal';
 	$('button.navbar-toggler').addClass('mt-5').removeClass('mt-3');
 	$('form#search-form').addClass('mt-5').removeClass('mt-3');
 };
-
 function resizeHeader () {
 	if ($('body').scrollTop() > 35 || $(window).width() < 576) {
 		headerSmall();
